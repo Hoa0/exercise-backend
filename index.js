16ac2cf17360e69f459fc620fa646902fb9bccde
hoa0 <t-hoa.l@hotmail.com>
2021-02-10 19:17:40 +0100

    add route and nodemon

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/movies', (req, res) => {
+    res.send('A list of movies')
+})
+app.get('/movies/:movieId', (req, res) => {
+    res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`)
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
